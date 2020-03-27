6741c5711b8f557065640475f729fa712aff7040
Jesus Ramirez <jesusdevelp@gmail.com>
2020-03-27 15:08:59 -0600

    Initial configuration and installation of dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, Router } from "react-router";
+import PropTypes from "prop-types";
+import "./App.css";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router history={this.props.history}>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          {/* Here you can put more routes on */}
+        </div>
+      </Router>
+    );
+  }
 }
+
+App.propTypes = {
+  history: PropTypes.any.isRequired
+};
 
 export default App;
