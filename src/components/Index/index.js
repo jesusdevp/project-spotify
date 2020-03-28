cc2af0080572281aae0a5accf48314878eea974f
Jesus Ramirez <jesusdevelp@gmail.com>
2020-03-28 11:23:09 -0600

    Search button added

--- src/components/Index/index.js
+++ src/components/Index/index.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 
+import "./index.css";
+import "materialize-css/dist/css/materialize.min.css";
+
 class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      song: ""
+    };
+  }
   render() {
-    return <div>Index</div>;
+    return (
+      <div className="Index">
+        <div className="card">
+          <div className="card-content">
+            <div className="Index-searchBox">
+              <input
+                type="text"
+                className="Inde-searchBox-input"
+                placeholder="Song"
+                value={this.state.song}
+                onChange={e => {
+                  this.setState({ song: e.target.value });
+                }}
+              />
+              <a className="waves-effect waves-light btn green">
+                <i class="fa fa-search"></i>
+              </a>
+              <div>{/* {TODO} */}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
